2a3a82d3655e732e11fbfd1fb269d3953ca0a886
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-03-28 16:04:11 -0500

    Capa service, en el metodo crearMensajes Creamos la accion para recopilar datos, crear el obeto mensaje, y enviar los datos de este objetos a la capa DAO

--- src/main/java/com/perisistencia/mensajes_p2/MensajesService.java
+++ src/main/java/com/perisistencia/mensajes_p2/MensajesService.java
@@ -4,15 +4,34 @@
  */
 package com.perisistencia.mensajes_p2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USER
  */
 public class MensajesService {
     // la intermedia entre el menu principal y la capa de acceso 
-    // comienzo
+    // comienzo 
+    
+    // Esta capa es la encargada de preguntarnos los datos para enviarcelos
+    // a la capa de mensajesDAO. 
     
     public static void creaMensaje() {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu Mensaje");
+        String mensaje = sc.nextLine(); 
+        
+        System.out.println("Autor: ");
+        String nombre = sc.nextLine(); 
+        
+        Mensajes registroMensajes = new Mensajes();
+        registroMensajes.setAutor_mensaje(nombre);
+        registroMensajes.setMensaje(mensaje);
+        
+        // Despues de creado el objeto, lo enviamos a la capa DAO
+        MensajesDAO.crecarMensajeDB(registroMensajes);
         
     }
     
